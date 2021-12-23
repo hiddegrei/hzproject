f06f94a5c08999a04fbc9f38fba9d28a9d191d5f
Jos-HZ <90176784+Jos-HZ@users.noreply.github.com>
2021-12-23 10:47:50 +0100

    idk

--- build/Start.js
+++ build/Start.js
@@ -1,21 +1,25 @@
 document.getElementById('start-btn').addEventListener('click', start);
 function start(param) {
-    let username = document.querySelector('input[name="username"]').value;
-    let password = document.querySelector('input[name="password"]').value;
-    if (username === '' && password === '') {
+    let username = document.querySelector('input[id="username"]').value;
+    let password = document.querySelector('input[id="password"]').value;
+    let repeatPassword = document.querySelector('input[id="repeatPassword"]').value;
+    if (username === '' && password === '' || username === '' && repeatPassword === '') {
         alert("Voer een gebruikersnaam en wachtwoord in!");
     }
     else if (username === '') {
         alert("Voer een gebruikersnaam in!");
     }
-    else if (password === '') {
+    else if (password === '' || repeatPassword === '') {
         alert("Voer een wachtwoord in!");
     }
-    else {
-        window.location.href = `./?username=${username}&password=${password}`;
+    else if (password === repeatPassword) {
+        window.location.href = `./start.html?username=${username}&password=${password}`;
+        let myStorage = window.localStorage;
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
     }
-    let myStorage = window.localStorage;
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
+    else {
+        alert("Wachtwoorden komen niet overeen!");
+    }
 }
 //# sourceMappingURL=Start.js.map
