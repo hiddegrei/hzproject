3f8c1626be7b0fba7219d2bfcdb79c827a2474eb
hiddegrei <grei0005@hz.nl>
2021-12-23 17:43:24 +0100

    agent borders

--- build/Border.js
+++ build/Border.js
@@ -10,14 +10,26 @@ export default class Border {
         this.type = type;
     }
     show() {
-        this.ctx.strokeStyle = "rgb(255,255,255)";
-        this.ctx.lineWidth = 1;
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.a.x, this.a.y);
-        this.ctx.lineTo(this.b.x, this.b.y);
-        this.ctx.closePath();
-        this.ctx.stroke();
-        this.ctx.fill();
+        if (this.type === "normal") {
+            this.ctx.strokeStyle = "rgb(255,255,255)";
+            this.ctx.lineWidth = 1;
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.a.x, this.a.y);
+            this.ctx.lineTo(this.b.x, this.b.y);
+            this.ctx.closePath();
+            this.ctx.stroke();
+            this.ctx.fill();
+        }
+        else {
+            this.ctx.strokeStyle = "rgb(0,0,255)";
+            this.ctx.lineWidth = 1;
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.a.x, this.a.y);
+            this.ctx.lineTo(this.b.x, this.b.y);
+            this.ctx.closePath();
+            this.ctx.stroke();
+            this.ctx.fill();
+        }
     }
 }
 //# sourceMappingURL=Border.js.map
--- src/Level1map.ts
+++ src/Level1map.ts
@@ -628,6 +628,9 @@ export default class Level1map {
               [(this.canvas.width/2),100+5*this.widthHall,(this.canvas.width/2)+this.widthHall,100+5*this.widthHall],
               [(this.canvas.width/2)-19*this.widthHall,100+13*this.widthHall,(this.canvas.width/2)-18*this.widthHall,100+13*this.widthHall],
               [(this.canvas.width/2)-10*this.widthHall,100+7*this.widthHall,(this.canvas.width/2)-10*this.widthHall,100+8*this.widthHall],
+
+              [(this.canvas.width/2)-14*this.widthHall,100+9*this.widthHall,(this.canvas.width/2)-14*this.widthHall,100+10*this.widthHall],
+              [(this.canvas.width/2)+11*this.widthHall,100,(this.canvas.width/2)+11*this.widthHall,100+this.widthHall],
           ]
       }
 
