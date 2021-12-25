d212d21cefb04ce3e68df2d3a81f0cadc74e1acc
Jos-HZ <90176784+Jos-HZ@users.noreply.github.com>
2021-12-25 16:16:05 +0100

    Added repeat password check and empty field check. Passwords must match and may not be empty. User must enter username. Will alert user if conditions aren't met.

--- build/Start.js
+++ build/Start.js
@@ -3,10 +3,22 @@ function start(param) {
     let username = document.querySelector('input[id="username"]')?.value;
     let password = document.querySelector('input[id="password"]')?.value;
     let repeatPassword = document.querySelector('input[id="repeatPassword"]')?.value;
-    let myStorage = window.localStorage;
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-    console.log(localStorage.getItem('username'));
-    console.log(localStorage.getItem('password'));
+    if (username === '' && password === '' || username === '' && repeatPassword === '') {
+        alert("Voer een gebruikersnaam en wachtwoord in!");
+    }
+    else if (username === '') {
+        alert("Voer een gebruikersnaam in!");
+    }
+    else if (password === '' || repeatPassword === '') {
+        alert("Voer een wachtwoord in!");
+    }
+    else if (password === repeatPassword) {
+        let myStorage = window.localStorage;
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    }
+    else {
+        alert("Wachtwoorden komen niet overeen!");
+    }
 }
 //# sourceMappingURL=Start.js.map
--- src/Start.ts
+++ src/Start.ts
@@ -3,10 +3,17 @@ function start(param: any) {
     let username=document.querySelector<HTMLInputElement>('input[id="username"]')?.value;
     let password=document.querySelector<HTMLInputElement>('input[id="password"]')?.value;
     let repeatPassword=document.querySelector<HTMLInputElement>('input[id="repeatPassword"]')?.value;
-
-    let myStorage = window.localStorage;
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-    console.log(localStorage.getItem('username'));
-    console.log(localStorage.getItem('password'));
+    if (username === '' && password === '' || username === '' && repeatPassword === '') {
+        alert("Voer een gebruikersnaam en wachtwoord in!");
+    } else if (username === '') {
+        alert("Voer een gebruikersnaam in!");
+    } else if (password === '' || repeatPassword === '') {
+        alert("Voer een wachtwoord in!");
+    } else if (password === repeatPassword) {
+        let myStorage = window.localStorage;
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    } else {
+        alert("Wachtwoorden komen niet overeen!");
+    }
 }
